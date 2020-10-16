65c11bd8e20b8697763845e8b1fcaec27ff4f232
Jakub Żuchowski <ellmunzai@gmail.com>
2020-10-16 23:47:58 +0200

    WIP 1

---
+++ app/services/breakpoint_calculator_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class BreakpointCalculatorService < BaseService
+  attribute :race, Types::Strict::Symbol
+  attribute :opponent, Types::Strict::Symbol
+
+  pipe :load_data do
+    DataLoaderService.new.call
+  end
+
+  pipe :assign_races do
+    data = last_result.with_indifferent_access
+
+    bump(:race) { data[race] }
+    bump(:opponent) { data[opponent] }
+  end
+
+  pipe :step_3 do
+    true
+  end
+end
