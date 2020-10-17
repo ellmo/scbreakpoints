5a41d64719e277b06054e6d8cf8d43331f03fc3a
Jakub Żuchowski <ellmunzai@gmail.com>
2020-10-17 02:17:40 +0200

    taking size and damage type into account

--- app/services/calculator/strike_count_service.rb
+++ app/services/calculator/strike_count_service.rb
@@ -1,12 +1,39 @@
 # frozen_string_literal: true
 
+# require "matrix"
+
 class Calculator::StrikeCountService < BaseService
+  COEFF_MATRIX = [[0.5, 0.75, 1.0], [0.25, 0.5, 1.0]].freeze
+  SIZES        = %w[small medium large].freeze
+  TYPES        = %w[explosive plasma].freeze
+  # COEFF_MATRIX = Matrix[[0.25, 0.5, 1.0], [0.5, 0.75, 1.0]].freeze
+
   attribute :damage,    Types::Strict::Integer
   attribute :attacks,   Types::Strict::Integer
   attribute :armor,     Types::Strict::Integer
   attribute :hitpoints, Types::Strict::Integer
+  attribute :size,      Types::Strict::String.default("small")
+  attribute :type,      Types::Strict::String.default("normal")
+
+  pipe :calculate_coefficient do
+    bump(:damage) { damage * coefficient }
+  end
 
   pipe :calculate_strikes do
     (hitpoints.to_f / (attacks * (damage - armor))).ceil
+  end
+
+  def coefficient
+    return 1.0 if type == "normal"
+
+    COEFF_MATRIX[type_i][size_i]
+  end
+
+  def size_i
+    SIZES.index size
+  end
+
+  def type_i
+    TYPES.index type
   end
 end
--- spec/services/calculator/strike_count_service_spec.rb
+++ spec/services/calculator/strike_count_service_spec.rb
@@ -12,7 +12,7 @@ describe Calculator::StrikeCountService do
     end
   end
 
-  context "pure Zealot vs pure Ling arguments passed" do
+  context "pure Zealot vs. pure Ling arguments" do
     let(:arguments) do
       { hitpoints: 35, damage: 8, attacks: 2, armor: 0 }
     end
@@ -26,13 +26,23 @@ describe Calculator::StrikeCountService do
     end
   end
 
-  context "pure Ling vs pure Marine arguments passed" do
+  context "pure Ling vs. pure Marine arguments" do
     let(:arguments) do
       { hitpoints: 40, damage: 5, attacks: 1, armor: 0 }
     end
 
     it "returns 3 strikes" do
       expect(subject.success).to eq 8
+    end
+  end
+
+  context "pure Sieged Siege Tank vs. pure Marine" do
+    let(:arguments) do
+      { hitpoints: 40, damage: 70, attacks: 1, armor: 0, type: "explosive" }
+    end
+
+    it "returns 2 strikes" do
+      expect(subject.success).to eq 2
     end
   end
 end
