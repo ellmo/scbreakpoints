a9bf21c5eea0f8273e42a7e2108974c7c36f669c
Jakub Żuchowski <ellmunzai@gmail.com>
2020-11-07 15:15:40 +0100

    url matching /:unit/:target/
    and setting up root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :home, only: [:index]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/:unit/:target" => "home#index"
+
+  root "home#index"
 end
