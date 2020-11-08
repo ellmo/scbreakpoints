5a456c474f28a11f5e929396278d9c2cb1049313
Jakub Żuchowski <ellmunzai@gmail.com>
2020-11-08 23:07:58 +0100

    clearing up the strike calculator

--- app/services/calculator/strike_count_service.rb
+++ app/services/calculator/strike_count_service.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
-# require "matrix"
+require "forwardable"
 
 class Calculator::StrikeCountService < BaseService
-  COEFF_MATRIX = [[0.5, 0.75, 1.0], [0.25, 0.5, 1.0]].freeze
-  SIZES        = %w[small medium large].freeze
-  TYPES        = %w[explosive plasma].freeze
-  # COEFF_MATRIX = Matrix[[0.25, 0.5, 1.0], [0.5, 0.75, 1.0]].freeze
-
   attribute :unit,          Types::Strict::String
   attribute :target,        Types::Strict::String
   attribute :bonus_attack,  Types::Strict::Integer.default(0)
   attribute :bonus_armor,   Types::Strict::Integer.default(0)
   attribute :bonus_shield,  Types::Strict::Integer.default(0)
+
+  extend Forwardable
+  def_delegators :attack, :attacks
+  def_delegators :target, :hitpoints, :shields
 
   pipe :fetch_data do
     bump(:unit)   { Unit.find_by name: unit }
@@ -37,9 +36,11 @@ class Calculator::StrikeCountService < BaseService
   end
 
   def coefficient
-    return 1.0 if unit.type_i.nil?
+    @coefficient ||= unit.coefficient_vs(target)
+  end
 
-    COEFF_MATRIX[unit.type_i][target.size_i]
+  def attack
+    @attack ||= unit.attack_vs(target)
   end
 
   def body_damage(actual_damage = damage)
@@ -47,22 +48,10 @@ class Calculator::StrikeCountService < BaseService
   end
 
   def damage
-    (unit.attack["ground"]["damage"]) + (bonus_attack * unit.attack["ground"]["bonus"])
-  end
-
-  def attacks
-    unit.attack["ground"]["attacks"]
-  end
-
-  def shields
-    target.shields || 0
+    attack.damage + (bonus_attack * attack.bonus)
   end
 
   def armor
     target.armor + bonus_armor
-  end
-
-  def hitpoints
-    target.hitpoints
   end
 end
