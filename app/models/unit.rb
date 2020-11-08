bcf61de83d4a6d3ec3a552367ab7323abec73a72
Jakub Żuchowski <ellmunzai@gmail.com>
2020-11-08 18:04:07 +0100

    saving slugs, finding by slugs

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,6 +2,9 @@ class Unit
   SIZES = %w[small medium large].freeze
   TYPES = %w[explosive plasma].freeze
 
+#======
+#= DOC
+#====
   include Mongoid::Document
   field :race,      type: String
   field :name,      type: String
@@ -15,13 +18,34 @@ class Unit
 
   field :attack,    type: Hash
 
+#============
+#= CALLBACKS
+#==========
+  before_save :add_name_to_slugs, -> { slugs_changed? }
+
+#==========
+#= METHODS
+#========
   include AttackData
 
+  def self.find(slug)
+    Unit.find_by slugs: slug
+  end
   def size_i
     SIZES.index size
   end
 
   def type_i
     TYPES.index attack["ground"]["type"]
+  end
+
+private
+
+  def add_name_to_slugs
+    if slugs.present? && slugs.index(name).nil?
+      slugs << name
+    else
+      self.slugs = [name]
+    end
   end
 end
