9e5a8aa1f962b8c0670a77ece76ed2e8737e7852
Jakub Żuchowski <ellmunzai@gmail.com>
2020-11-14 20:21:38 +0100

    helpers to simplify things
    they are ugly helpers, tho

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,25 @@
 module HomeHelper
+  def race_selector(name, mine: nil, theirs: nil)
+    content_tag :li, class: ["tabs-title", active_element?(name, mine || theirs)] do
+      link_to "", "##{mine ? 'my' : 'their'}-#{name}",
+              class:           [name],
+              "aria-selected": race_selected?(name, mine || theirs)
+    end
+  end
+
+  def race_selected(arg)
+    return arg.name if arg.is_a?(Race)
+    return arg.race if arg.is_a?(Unit)
+
+    nil
+  end
+
+  def mine?
+
+  end
+
   def race_selected?(race, arg)
-    (arg.is_a?(Race) && arg.name == race) ||
-      (arg.is_a?(Unit) && arg.race == race)
+    race_selected(arg) == race
   end
 
   def active_element?(race, arg)
