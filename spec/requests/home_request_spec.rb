01e7005eebe16b1f486662d3cb7fad44c4cedb9f
Jakub Żuchowski <ellmunzai@gmail.com>
2020-11-21 13:36:59 +0100

    spec fix

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -2,10 +2,36 @@ require "rails_helper"
 
 RSpec.describe "Home", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/"
+    context "no params" do
+      it "returns http success" do
+        get "/"
 
-      expect(response).to have_http_status(:success)
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "wrong params" do
+      it "returns http success" do
+        get "/dupa/kupa"
+
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "slightly wrong params" do
+      it "returns http success" do
+        get "/ling/protos"
+
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "proper params" do
+      it "returns http success" do
+        get "/ling/protoss"
+
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
