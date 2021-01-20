afe77c98765188a21c23cd61e3e58f647b409fab
Jakub Żuchowski <ellmunzai@gmail.com>
2021-01-20 19:24:55 +0100

    a different approach to benchmarks
    ~20k/s seems more realistic

--- spec/services/simulation_service_spec.rb
+++ spec/services/simulation_service_spec.rb
@@ -21,17 +21,37 @@ describe SimulationService do
       expect(subject).to be_success
     end
 
-    it "Zealot wins in 4 vs a gazzilion of attacks" do
+    it "Zealot wins in 4 vs 7 strikes" do
       expect(subject.success).to eq "zealot wins with 3 strikes against 7"
     end
+  end
 
-    describe "benchmark" do
-      it do
-        Benchmark.ips do |bench|
-          bench.report("Ling vs Zealot") { subject }
+  context "Goon vs Tank (tank mode)" do
+    let(:arguments) { { red: Unit.find(:goon), blue: Unit.find(:tank), report: true } }
 
-          # bench.compare!
+    it "is a success" do
+      expect(subject).to be_success
+    end
+
+    it "Tank wins in 7 vs 7 strikes" do
+      expect(subject.success).to eq "siege_tank_tank_mode wins with 7 strikes against 7"
+    end
+  end
+
+  describe "benchmark" do
+    ling_zealot_arguments = { red: Unit.find(:ling), blue: Unit.find(:zealot) }
+    goon_tank_arguments = { red: Unit.find(:dragoon), blue: Unit.find(:tank) }
+
+    it do
+      Benchmark.ips do |bench|
+        bench.report("Ling vs Zealot") do
+          described_class.new(ling_zealot_arguments).call
         end
+        bench.report("Dragoon vs Tank") do
+          described_class.new(goon_tank_arguments).call
+        end
+
+        bench.compare!
       end
     end
   end
