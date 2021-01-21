6a810643e9bad6fac80f49b1c22c3ff7e6d8f952
Jakub Żuchowski <ellmunzai@gmail.com>
2021-01-21 01:58:22 +0100

    fixing spec

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -2,25 +2,27 @@ require "rails_helper"
 
 describe Unit, type: :model do
   describe "#coefficient" do
-    subject { unit.coefficient target }
+    subject { red.coefficient }
+
+    before { red.target!(blue) }
 
     context "zealot attacks zergling" do
-      let(:unit)   { Unit.find "zealot" }
-      let(:target) { Unit.find "zergling" }
+      let(:red)  { Unit.find "zealot" }
+      let(:blue) { Unit.find "zergling" }
 
       it { is_expected.to eq 1.0 }
     end
 
     context "zealot attacks mutalisk" do
-      let(:unit)   { Unit.find "zealot" }
-      let(:target) { Unit.find "mutalisk" }
+      let(:red)  { Unit.find "zealot" }
+      let(:blue) { Unit.find "mutalisk" }
 
       it { is_expected.to eq nil }
     end
 
     context "dragoon attacks mutalisk" do
-      let(:unit)   { Unit.find "dragoon" }
-      let(:target) { Unit.find "mutalisk" }
+      let(:red)  { Unit.find "dragoon" }
+      let(:blue) { Unit.find "mutalisk" }
 
       it { is_expected.to eq 0.5 }
     end
