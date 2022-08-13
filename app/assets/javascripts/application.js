ad920ae25fce0e261623a311d5182eceba3f3396
Jakub Żuchowski <ellmunzai@gmail.com>
2022-08-13 22:36:06 +0200

    and now JS-function simplified

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,41 +17,24 @@ function parsed_url(){
 function get_combatants(){
   $.get(parsed_url(), function(data, _status){
 
-    uncover_mine(data.attacker);
-    uncover_theirs(data.target);
+    uncover("my", data.attacker);
+    uncover("theirs", data.target);
   })
 }
 
-function uncover_mine(id){
-  $(`#my-content .tabs-panel`)
-    .removeClass('is-active');
-  $(`#my-tabs li`)
-    .removeClass('is-active');
+function uncover(side, id){
+  $(`#${side}-content .tabs-panel`)
+    .removeClass("is-active");
+  $(`#${side}-tabs li`)
+    .removeClass("is-active");
 
-  race = $(`div#my-content a[data-unit-id=${id}]`)
-    .addClass('selected')
+  content = $(`div#${side}-content a[data-unit-id=${id}]`)
+    .addClass("selected")
     .closest(`.tabs-panel`)
     .addClass(`is-active`)
-    .attr('id')
+    .attr("id")
 
-  $(`a#${race}-label`)
+  $(`a#${content}-label`)
     .parent()
-    .addClass('is-active');
-}
-
-function uncover_theirs(id){
-  $(`#their-content .tabs-panel`)
-    .removeClass('is-active');
-  $(`#theirs-tabs li`)
-    .removeClass('is-active');
-
-  race = $(`div#theirs-content a[data-unit-id=${id}]`)
-    .addClass('selected')
-    .closest(`.tabs-panel`)
-    .addClass(`is-active`)
-    .attr('id')
-
-  $(`a#${race}-label`)
-    .parent()
-    .addClass('is-active');
+    .addClass("is-active");
 }
