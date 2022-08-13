74be158464de1ea91deff45027505ce088f4cd69
Jakub Żuchowski <ellmunzai@gmail.com>
2022-08-13 17:05:04 +0200

    a start of something great, also JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,24 @@
 //= require foundation
 
-$(function(){ $(document).foundation(); });
+$(function(){
+  $(document).foundation();
+
+  attacker = null;
+  target   = null;
+
+  get_combatants();
+});
+
+parsed_url = function(){
+  path   = "/api/parse"
+  params = window.location.pathname;
+
+  return(path + params);
+}
+
+get_combatants = function() {
+  $.get(parsed_url(), function(data, _status){
+    attacker = data.attacker;
+    target   = data.target;
+  })
+}
